63340fe38d6c6439c7938c9c4ccc00b907d346c9
Hiro <hiro@inkri.com>
2017-01-28 08:09:55 +0000

    DIVE14-KADAI-config.consider_all_requests_local = false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -42,7 +42,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
 
-  config.web_console.whitelisted_ips = '10.0.2.2'
-  
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
